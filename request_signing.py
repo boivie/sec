2f514b09e73d9f7cc30576549c40576e866cf094
Victor Boivie <victor@boivie.com>
2015-06-14 17:44:52 +0200

    request sining stuff

--- request_signing.py
+++ request_signing.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 import base64
 import hashlib
-import json
 import sys
 from collections import OrderedDict
 
 import requests
-import jws
-import Crypto.PublicKey.RSA as rsa
+import jwt
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
+from cryptography.hazmat.backends import default_backend
 
 
 def b64_encode(source):
@@ -38,26 +38,26 @@ private_key_fname = sys.argv[1]
 fingerprint = sys.argv[2]
 base_url = sys.argv[3]
 
-priv = rsa.importKey(open(private_key_fname))
+payload = {'iss': 'victor', 'exp': 15,
+           'claim': 'insanity'}
 
+priv = load_pem_private_key(open(private_key_fname, 'r').read(),
+                            password=None, backend=default_backend())
 r = requests.post(base_url + "/request/").json()
 req_id = r['id']
 req_url = r['url']
 
-h1  = {'alg': 'RS256', 'typ': "create",
-       'x5t#S256': fingerprint}
-p1 = OrderedDict([('header', {}),
-                  ('request_id', req_id)])
-s1 = jws._signing_input(h1, p1) + "." + jws.sign(h1, p1, priv)
-id1 = b64_encode(hashlib.sha256(s1).digest())
 
-h2  = {'alg': 'RS256', 'typ': "loginreq",
-       'x5t#S256': fingerprint}
-p2 = OrderedDict([('header', dict(parent=id1)),
-                  ('identity', 'user@example.com'),
-                  ('target', 'example.com'),
-                  ('reference', '902ba2ac8')])
-s2 = jws._signing_input(h2, p2) + "." + jws.sign(h2, p2, priv)
-
-requests.post(req_url, json=dict(records=[s1, s2]))
+payload = {
+    "aud": "Sec",
+    "iss": "host.example.com",
+    "typ": "boivie/sec/login/v1",
+    "request": {
+        "reference": "902ba2ac8",
+        "username": "user@host.example.com"
+    }
+}
+jwt_message = jwt.encode(payload, priv, algorithm='RS256')
+print(jwt_message)
+requests.post(req_url, json=dict(jwt=jwt_message))
 print(req_url)
